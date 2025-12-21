c4f766f92af89e59ff055ae85d79609a18e412af
RobiEcho <firefullover@gmail.com>
2025-12-21 22:42:56 +0800

    Refactor the WIFI Configuration Code

---
+++ components/wifi/include/wifi_config.h
@@ -0,0 +1,25 @@
+#ifndef __WIFI_CONFIG_H__
+#define __WIFI_CONFIG_H__
+
+#include "esp_wifi_types.h"
+
+#define WIFI_APP_MODE_STA   1
+#define WIFI_APP_MODE_AP    2
+
+#ifndef WIFI_APP_MODE
+#define WIFI_APP_MODE WIFI_APP_MODE_STA
+#endif
+
+/* ==================== STA Config ==================== */
+#define WIFI_STA_SSID          "DragonG"
+#define WIFI_STA_PASS          "lrt13729011089"
+#define WIFI_STA_AUTHMODE      WIFI_AUTH_WPA2_PSK
+
+/* ==================== AP Config ===================== */
+#define WIFI_AP_SSID           "ESP32S3_AP"
+#define WIFI_AP_PASS           "12345678"
+#define WIFI_AP_AUTHMODE       WIFI_AUTH_WPA_WPA2_PSK
+#define WIFI_AP_CHANNEL        1
+#define WIFI_AP_MAX_CONN       4
+
+#endif /* __WIFI_CONFIG_H__ */
